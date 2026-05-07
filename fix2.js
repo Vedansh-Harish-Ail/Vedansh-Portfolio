7b48c845e194551fa27a92dde50c59a5b139b834
Vedansh-Harish-Ail <ailvedansh@gmail.com>
2026-05-07 09:44:46 +0530

    refactor: remove temporary build scripts for HTML asset migration

--- fix2.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let f = fs.readFileSync('index.html', 'utf8');
-f = f.replace(/class=\"([^\"]*?)text-white([^\"]*?)\"/g, 'class="$1text-on-primary$2"');
-fs.writeFileSync('index.html', f);
-console.log('done text-white replacement');
